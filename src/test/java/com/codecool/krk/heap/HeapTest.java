2cfc4f03b102179628bcd480efcca3f869ab4146
Paprok <rafal.becla@gmail.com>
2019-02-01 21:13:34 +0100

    created enqueue test

--- src/test/java/com/codecool/krk/heap/HeapTest.java
+++ src/test/java/com/codecool/krk/heap/HeapTest.java
@@ -1,4 +1,20 @@
 package com.codecool.krk.heap;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 public class HeapTest {
+
+    @Test
+    void testAddListToHeap() {
+        int[] list = {2, 7, 26, 25, 19, 17, 1, 90, 3, 36};
+        Heap heap = new Heap(list.length);
+        heap.addList(list);
+
+        int[] actual = heap.getHeap();
+        int[] expected = {90, 36, 17, 25, 26, 7, 1, 2, 3, 19};
+
+        assertArrayEquals(expected, actual);
+    }
 }
