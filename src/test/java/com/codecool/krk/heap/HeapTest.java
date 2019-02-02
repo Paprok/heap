8edda44ca0a30518ce59c16286c587d9c5345f4c
Paprok <rafal.becla@gmail.com>
2019-02-02 15:16:10 +0100

    created dequeue tests

--- src/test/java/com/codecool/krk/heap/HeapTest.java
+++ src/test/java/com/codecool/krk/heap/HeapTest.java
@@ -2,7 +2,7 @@ package com.codecool.krk.heap;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class HeapTest {
 
@@ -14,6 +14,41 @@ public class HeapTest {
 
         int[] actual = heap.getHeap();
         int[] expected = {90, 36, 17, 25, 26, 7, 1, 2, 3, 19};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void testDequeueOnEmptyHeapThrowsException() {
+        Heap heap = new Heap(5);
+
+        assertThrows(HeapUnderflow.class,
+
+                () -> heap.dequeue()
+        );
+    }
+
+    @Test
+    void testDequeueReturnsValidInt() {
+        int[] list = {2, 7, 26, 25, 19, 17, 1, 90, 3, 36};
+        Heap heap = new Heap(list.length);
+        heap.addList(list);
+
+        int expected = 90;
+        int actual = heap.dequeue();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testHeapReturnsValidListAfterDequeue() {
+        int[] list = {2, 7, 26, 25, 19, 17, 1, 90, 3, 36};
+        Heap heap = new Heap(list.length);
+        heap.addList(list);
+        heap.dequeue();
+
+        int[] actual = heap.getHeap();
+        int[] expected = {36, 26, 17, 25, 19, 7, 1, 2, 3};
 
         assertArrayEquals(expected, actual);
     }
